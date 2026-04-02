acb9e886d1e485bc4d846cb6313145e225c95baf
Hamza Iqbal <hamzaiqbal1109@gmail.com>
2026-04-02 22:33:41 +0500

    Clean plugin structure

--- gyroscope_plugin/lib/gyroscope_plugin.dart
+++
@@ -1,8 +0,0 @@
-
-import 'gyroscope_plugin_platform_interface.dart';
-
-class GyroscopePlugin {
-  Future<String?> getPlatformVersion() {
-    return GyroscopePluginPlatform.instance.getPlatformVersion();
-  }
-}
